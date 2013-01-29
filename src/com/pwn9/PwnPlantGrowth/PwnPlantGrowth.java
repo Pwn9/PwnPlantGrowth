7acb50d63a32e541691fda975930f2e9e1a0ec4d
tremor <admin@pwn9.com>
2013-01-29 07:53:19 -0500

    Code cleanup.

--- src/com/pwn9/PwnPlantGrowth/PwnPlantGrowth.java
+++ src/com/pwn9/PwnPlantGrowth/PwnPlantGrowth.java
@@ -8,97 +8,99 @@ import org.bukkit.TreeType;
 import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockGrowEvent;
 import org.bukkit.event.world.StructureGrowEvent;
+import org.bukkit.inventory.ItemStack;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PwnPlantGrowth extends JavaPlugin implements Listener {
+	
 	public ArrayList<Integer> softBlocks = new ArrayList<Integer>();
+	
+	static Random randomNumberGenerator = new Random();
 	
 	public void onEnable() {
 		this.saveDefaultConfig();
 		this.getServer().getPluginManager().registerEvents(this, this);
+
 	}
 	
 	public void onDisable() {
 		
 	}
 	
-	@EventHandler
+	@EventHandler(ignoreCancelled = true)
+	public void onBlockBreak(BlockBreakEvent breakEvent) {
+		if(breakEvent.getBlock().getType() == Material.NETHER_WARTS) {
+			if (PwnPlantGrowth.random(50)) {
+				//breakEvent.getBlock().getDrops().clear();
+				breakEvent.setCancelled(true);
+				breakEvent.getBlock().setType(Material.AIR);
+				
+			}
+		}
+	}
+	
+	@EventHandler(ignoreCancelled = true)
 	public void plantGrow(BlockGrowEvent e) {
 		//System.out.println("Growing "+ e.getBlock().getType().toString());
 		if (e.getBlock().getType() == Material.CARROT){
-			Random random = new Random();
-			int randomInt = random.nextInt(100);
-	    	String Chance = getConfig().getString("CARROT");
-			if (randomInt > Integer.parseInt(Chance)) {
+			
+			
+	    	int Chance = getConfig().getInt("CARROT");
+			if (PwnPlantGrowth.random(Chance)) {
 				e.setCancelled(true);
 			}			
 		}
 		// not yet supported by bukkit but maybe soon
 		if (e.getBlock().getType() == Material.COCOA){
-			Random random = new Random();
-			int randomInt = random.nextInt(100);
-	    	String Chance = getConfig().getString("COCOA");
-			if (randomInt > Integer.parseInt(Chance)) {	
+	    	int Chance = getConfig().getInt("COCOA");
+			if (PwnPlantGrowth.random(Chance)) {	
 					e.setCancelled(true);
 			}			
 		}	
 		if (e.getBlock().getType() == Material.CROPS){
-			Random random = new Random();
-			int randomInt = random.nextInt(100);
-	    	String Chance = getConfig().getString("WHEAT");
-			if (randomInt > Integer.parseInt(Chance)) {	
+	    	int Chance = getConfig().getInt("WHEAT");
+			if (PwnPlantGrowth.random(Chance)) {	
 				e.setCancelled(true);
 			}			
 		}			
 		if (e.getBlock().getType() == Material.MELON_STEM){
-			Random random = new Random();
-			int randomInt = random.nextInt(100);
-	    	String Chance = getConfig().getString("MELON_STEM");
-			if (randomInt > Integer.parseInt(Chance)) {	
+	    	int Chance = getConfig().getInt("MELON_STEM");
+			if (PwnPlantGrowth.random(Chance)) {	
 				e.setCancelled(true);
 			}			
 		}			
 		if (e.getBlock().getType() == Material.NETHER_WARTS){
-			Random random = new Random();
-			int randomInt = random.nextInt(100);
-	    	String Chance = getConfig().getString("NETHERWART");
-			if (randomInt > Integer.parseInt(Chance)) {	
+	    	int Chance = getConfig().getInt("NETHERWART");
+			if (PwnPlantGrowth.random(Chance)) {	
 				e.setCancelled(true);
 			}			
 		}	
 		if (e.getBlock().getType() == Material.POTATO){
-			Random random = new Random();
-			int randomInt = random.nextInt(100);
-	    	String Chance = getConfig().getString("POTATO");
-			if (randomInt > Integer.parseInt(Chance)) {	
+	    	int Chance = getConfig().getInt("POTATO");
+			if (PwnPlantGrowth.random(Chance)) {	
 				e.setCancelled(true);
 			}			
 		}			
 		if (e.getBlock().getType() == Material.PUMPKIN_STEM){
-			Random random = new Random();
-			int randomInt = random.nextInt(100);
-	    	String Chance = getConfig().getString("PUMPKIN_STEM");
-			if (randomInt > Integer.parseInt(Chance)) {
+	    	int Chance = getConfig().getInt("PUMPKIN_STEM");
+			if (PwnPlantGrowth.random(Chance)) {
 				e.setCancelled(true);
 			}			
 		}	
 		if (e.getBlock().getType() == Material.AIR){
 			if (e.getBlock().getRelative(BlockFace.DOWN).getType() == Material.CACTUS) {
-				Random random = new Random();
-				int randomInt = random.nextInt(100);
-		    	String Chance = getConfig().getString("CACTUS");
-				if (randomInt > Integer.parseInt(Chance)) {
+		    	int Chance = getConfig().getInt("CACTUS");
+				if (PwnPlantGrowth.random(Chance)) {
 					e.setCancelled(true);
 				}				
 			}
 			else if (e.getBlock().getRelative(BlockFace.DOWN).getType() == Material.SUGAR_CANE_BLOCK) {
-				Random random = new Random();
-				int randomInt = random.nextInt(100);
-		    	String Chance = getConfig().getString("SUGAR_CANE");
-				if (randomInt > Integer.parseInt(Chance)) {
+		    	int Chance = getConfig().getInt("SUGAR_CANE");
+				if (PwnPlantGrowth.random(Chance)) {
 					e.setCancelled(true);
 				}				
 			}
@@ -106,10 +108,8 @@ public class PwnPlantGrowth extends JavaPlugin implements Listener {
 					(e.getBlock().getRelative(BlockFace.WEST).getType() == Material.MELON_STEM) ||
 					(e.getBlock().getRelative(BlockFace.NORTH).getType() == Material.MELON_STEM) ||
 					(e.getBlock().getRelative(BlockFace.SOUTH).getType() == Material.MELON_STEM)) {
-				Random random = new Random();
-				int randomInt = random.nextInt(100);
-		    	String Chance = getConfig().getString("MELON");
-				if (randomInt > Integer.parseInt(Chance)) {
+		    	int Chance = getConfig().getInt("MELON");
+				if (PwnPlantGrowth.random(Chance)) {
 					e.setCancelled(true);
 				}				
 			}
@@ -117,10 +117,8 @@ public class PwnPlantGrowth extends JavaPlugin implements Listener {
 					(e.getBlock().getRelative(BlockFace.WEST).getType() == Material.PUMPKIN_STEM) ||
 					(e.getBlock().getRelative(BlockFace.NORTH).getType() == Material.PUMPKIN_STEM) ||
 					(e.getBlock().getRelative(BlockFace.SOUTH).getType() == Material.PUMPKIN_STEM)) {
-				Random random = new Random();
-				int randomInt = random.nextInt(100);
-		    	String Chance = getConfig().getString("PUMPKIN");
-				if (randomInt > Integer.parseInt(Chance)) {
+		    	int Chance = getConfig().getInt("PUMPKIN");
+				if (PwnPlantGrowth.random(Chance)) {
 					e.setCancelled(true);
 				}				
 			}			
@@ -131,92 +129,75 @@ public class PwnPlantGrowth extends JavaPlugin implements Listener {
 	public void structureGrow(StructureGrowEvent e) {	
 		//System.out.println("Structure " +e.getSpecies().toString());	
 		if (e.getSpecies() == TreeType.BIG_TREE){
-			Random random = new Random();
-			int randomInt = random.nextInt(100);
-	    	String Chance = getConfig().getString("BIG_TREE");
-			if (randomInt > Integer.parseInt(Chance)) {
+	    	int Chance = getConfig().getInt("BIG_TREE");
+			if (PwnPlantGrowth.random(Chance)) {
 				e.setCancelled(true);
 			}
 		}
 		if (e.getSpecies() == TreeType.BIRCH){
-			Random random = new Random();
-			int randomInt = random.nextInt(100);
-	    	String Chance = getConfig().getString("BIRCH");
-			if (randomInt > Integer.parseInt(Chance)) {
+	    	int Chance = getConfig().getInt("BIRCH");
+			if (PwnPlantGrowth.random(Chance)) {
 				e.setCancelled(true);
 			}
 		}
 		if (e.getSpecies() == TreeType.BROWN_MUSHROOM){
-			Random random = new Random();
-			int randomInt = random.nextInt(100);
-	    	String Chance = getConfig().getString("BROWN_MUSHROOM");
-			if (randomInt > Integer.parseInt(Chance)) {
+	    	int Chance = getConfig().getInt("BROWN_MUSHROOM");
+			if (PwnPlantGrowth.random(Chance)) {
 				e.setCancelled(true);
 			}
 		}
 		if (e.getSpecies() == TreeType.JUNGLE){
-			Random random = new Random();
-			int randomInt = random.nextInt(100);
-	    	String Chance = getConfig().getString("JUNGLE");
-			if (randomInt > Integer.parseInt(Chance)) {
+	    	int Chance = getConfig().getInt("JUNGLE");
+			if (PwnPlantGrowth.random(Chance)) {
 				e.setCancelled(true);
 			}
 		}
 		if (e.getSpecies() == TreeType.JUNGLE_BUSH){
-			Random random = new Random();
-			int randomInt = random.nextInt(100);
-	    	String Chance = getConfig().getString("JUNGLE");
-			if (randomInt > Integer.parseInt(Chance)) {
+	    	int Chance = getConfig().getInt("JUNGLE");
+			if (PwnPlantGrowth.random(Chance)) {
 				e.setCancelled(true);
 			}
 		}
 		if (e.getSpecies() == TreeType.RED_MUSHROOM){
-			Random random = new Random();
-			int randomInt = random.nextInt(100);
-	    	String Chance = getConfig().getString("RED_MUSHROOM");
-			if (randomInt > Integer.parseInt(Chance)) {
+	    	int Chance = getConfig().getInt("RED_MUSHROOM");
+			if (PwnPlantGrowth.random(Chance)) {
 				e.setCancelled(true);
 			}
 		}
 		if (e.getSpecies() == TreeType.REDWOOD){
-			Random random = new Random();
-			int randomInt = random.nextInt(100);
-	    	String Chance = getConfig().getString("SPRUCE");
-			if (randomInt > Integer.parseInt(Chance)) {
+	    	int Chance = getConfig().getInt("SPRUCE");
+			if (PwnPlantGrowth.random(Chance)) {
 				e.setCancelled(true);
 			}
 		}
 		if (e.getSpecies() == TreeType.SMALL_JUNGLE){
-			Random random = new Random();
-			int randomInt = random.nextInt(100);
-	    	String Chance = getConfig().getString("JUNGLE");
-			if (randomInt > Integer.parseInt(Chance)) {
+	    	int Chance = getConfig().getInt("JUNGLE");
+			if (PwnPlantGrowth.random(Chance)) {
 				e.setCancelled(true);
 			}
 		}
 		if (e.getSpecies() == TreeType.SWAMP){
-			Random random = new Random();
-			int randomInt = random.nextInt(100);
-	    	String Chance = getConfig().getString("TREE");
-			if (randomInt > Integer.parseInt(Chance)) {
+	    	int Chance = getConfig().getInt("TREE");
+			if (PwnPlantGrowth.random(Chance)) {
 				e.setCancelled(true);
 			}
 		}
 		if (e.getSpecies() == TreeType.TALL_REDWOOD){
-			Random random = new Random();
-			int randomInt = random.nextInt(100);
-	    	String Chance = getConfig().getString("SPRUCE");
-			if (randomInt > Integer.parseInt(Chance)) {
+	    	int Chance = getConfig().getInt("SPRUCE");
+			if (PwnPlantGrowth.random(Chance)) {
 				e.setCancelled(true);
 			}
 		}
 		if (e.getSpecies() == TreeType.TREE){
-			Random random = new Random();
-			int randomInt = random.nextInt(100);
-	    	String Chance = getConfig().getString("TREE");
-			if (randomInt > Integer.parseInt(Chance)) {
+	    	int Chance = getConfig().getInt("TREE");
+			if (PwnPlantGrowth.random(Chance)) {
 				e.setCancelled(true);
 			}
 		}		
 	}
+	
+	static boolean random(int percentChance) {
+			return randomNumberGenerator.nextInt(100) < percentChance;
+	}
+	
 }
