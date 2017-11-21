bed40839383da589cfbc24a563cf94bccdbded77
Matt Sudol <matt.sudol@anaren.com>
2017-11-20 23:30:44 -0500

    is this the better way to do this?

--- src/com/pwn9/PwnPlantGrowth/Config.java
+++ src/com/pwn9/PwnPlantGrowth/Config.java
@@ -1,44 +1,44 @@
 package com.pwn9.PwnPlantGrowth;
 
-public class Config extends PwnPlantGrowth
+public class Config
 {
 	public static void LoadConfig()
 	{
 		// Get enabled worlds
-		PwnPlantGrowth.enabledWorlds = instance.getConfig().getStringList("enabled_worlds");
+		PwnPlantGrowth.enabledWorlds = PwnPlantGrowth.instance.getConfig().getStringList("enabled_worlds");
 		
 		// Get water source setting
-		PwnPlantGrowth.blockWaterBucket = instance.getConfig().getBoolean("block_water_bucket", false);
+		PwnPlantGrowth.blockWaterBucket = PwnPlantGrowth.instance.getConfig().getBoolean("block_water_bucket", false);
 		
 		// Get water source setting
-		PwnPlantGrowth.blockWaterDispenser = instance.getConfig().getBoolean("block_water_dispenser", false);
+		PwnPlantGrowth.blockWaterDispenser = PwnPlantGrowth.instance.getConfig().getBoolean("block_water_dispenser", false);
 		
 		// Get logfile setting
-		PwnPlantGrowth.logEnabled = instance.getConfig().getBoolean("debug_log", false);
+		PwnPlantGrowth.logEnabled = PwnPlantGrowth.instance.getConfig().getBoolean("debug_log", false);
 		
 		// Get Natural Light setting
-		PwnPlantGrowth.naturalLight = instance.getConfig().getInt("min_natural_light", 10);
+		PwnPlantGrowth.naturalLight = PwnPlantGrowth.instance.getConfig().getInt("min_natural_light", 10);
 		
 		// Dark growth
-		PwnPlantGrowth.darkGrow = instance.getConfig().getStringList("grow_in_dark");
+		PwnPlantGrowth.darkGrow = PwnPlantGrowth.instance.getConfig().getStringList("grow_in_dark");
 
 		// Weed Killer
-		PwnPlantGrowth.wkradius = instance.getConfig().getInt("weed_killer_radius", 5);
-		PwnPlantGrowth.weedKiller = instance.getConfig().getString("weed_killer", "GOLD_BLOCK");
-		PwnPlantGrowth.wkenabled = instance.getConfig().getBoolean("weed_killer_enabled", false);
+		PwnPlantGrowth.wkradius = PwnPlantGrowth.instance.getConfig().getInt("weed_killer_radius", 5);
+		PwnPlantGrowth.weedKiller = PwnPlantGrowth.instance.getConfig().getString("weed_killer", "GOLD_BLOCK");
+		PwnPlantGrowth.wkenabled = PwnPlantGrowth.instance.getConfig().getBoolean("weed_killer_enabled", false);
 
 		// Fertilizer
-		PwnPlantGrowth.fradius = instance.getConfig().getInt("fertilizer_radius", 5);
-		PwnPlantGrowth.frate = instance.getConfig().getInt("fertilizer_rate", 100);
-		PwnPlantGrowth.fertilizer = instance.getConfig().getString("fertilizer", "SOUL_SAND");
-		PwnPlantGrowth.fenabled = instance.getConfig().getBoolean("fertilizer_enabled", false);
+		PwnPlantGrowth.fradius = PwnPlantGrowth.instance.getConfig().getInt("fertilizer_radius", 5);
+		PwnPlantGrowth.frate = PwnPlantGrowth.instance.getConfig().getInt("fertilizer_rate", 100);
+		PwnPlantGrowth.fertilizer = PwnPlantGrowth.instance.getConfig().getString("fertilizer", "SOUL_SAND");
+		PwnPlantGrowth.fenabled = PwnPlantGrowth.instance.getConfig().getBoolean("fertilizer_enabled", false);
 		
 		// UV
-		PwnPlantGrowth.uvradius = instance.getConfig().getInt("uv_radius", 5);
-		PwnPlantGrowth.uv = instance.getConfig().getString("uv", "GLOWSTONE");
-		PwnPlantGrowth.uvenabled = instance.getConfig().getBoolean("uv_enabled", false);
+		PwnPlantGrowth.uvradius = PwnPlantGrowth.instance.getConfig().getInt("uv_radius", 5);
+		PwnPlantGrowth.uv = PwnPlantGrowth.instance.getConfig().getString("uv", "GLOWSTONE");
+		PwnPlantGrowth.uvenabled = PwnPlantGrowth.instance.getConfig().getBoolean("uv_enabled", false);
 		
 		// Bonemeal setting
-		PwnPlantGrowth.uvenabled = instance.getConfig().getBoolean("limit_bonemeal", false);		
+		PwnPlantGrowth.uvenabled = PwnPlantGrowth.instance.getConfig().getBoolean("limit_bonemeal", false);		
 	}
 }
